e27d6ec25ed4adc7644f47ee595497cc39f6dc01
jamie7dev <jamie7dev@gmail.com>
2023-01-23 22:22:05 +0900

    feature: add sort buttons

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
-  <Modal @closeModal="modalOpen = false" :rooms="rooms" :clickedId="clickedId" :modalOpen="modalOpen"/>
+
+  <!-- <div class="start" :class="{end : modalOpen}">
+    <Modal @closeModal="modalOpen = false" :rooms="rooms" :clickedId="clickedId" :modalOpen="modalOpen"/>
+  </div> -->
+
+  <transition name="modalTransition">
+    <Modal @closeModal="modalOpen = false" :rooms="rooms" :clickedId="clickedId" :modalOpen="modalOpen"/>
+  </transition>
+
+  
+  
   <div class="nav">
     <a v-for="(a, i) in navs" :key="i">{{a}}</a>
   </div>
 
-  <Discount />
+  <Discount v-if="showDiscount === true"/>
+
+  <button @click="priceSort">낮은 가격순</button>
+  <button @click="higherPrice">높은 가격순</button>
+  <button @click="titleSort">가나다순</button>
+  <button @click="sortBack">되돌리기</button>
+  <!-- <button @click="cheapItem">50만원 이하 상품</button> -->
+  
 
   <Card @openModal="modalOpen = true; clickedId=$event" :room="rooms[i]" v-for="(a, i) in rooms" :key="i"/>
   
@@ -31,16 +48,58 @@ export default {
     return{
       clickedId : 0,
       rooms : data, 
+      rooms_original : [...data],
       modalOpen : false,
       navs : ['Home', 'Products', 'About'],
       reports: 0,
+
+      showDiscount : true,
     }
   },
   methods : {
     // increase(){
     //   this.reports += 1;
     // }
+    priceSort(){
+      this.rooms.sort(function(a, b){
+        return a.price - b.price;
+      });
+    },
+    
+    higherPrice(){
+      this.rooms.sort(function(a, b){
+        return b.price - a.price;
+      })
+    },
+    titleSort(){
+      this.rooms.sort(function(a, b){
+        if(a.title < b.title){
+          return -1;
+        }
+        if(a.title > b.title){
+          return 1;
+        }
+      });
+    },
+    cheapItem(){
+      let cheap = this.rooms.filter( this.rooms.price <= 500000);
+      return cheap;
+    },
+    sortBack(){
+      this.rooms = [...this.rooms_original];
+    },
   },
+
+  created(){
+    //서버에서 데이터 가져오는 코드 ajax
+  },
+
+  // mounted(){
+  //   setTimeout(() => {
+  //     this.showDiscount = false;
+  //   }, 2000);
+  // },
+
   components: {
     Discount: Discount,
     Modal: Modal,
@@ -50,6 +109,31 @@ export default {
 </script>
 
 <style>
+.start {
+  opacity : 0;
+  transition: all 1s;
+}
+.end {
+  opacity: 1;
+}
+.modalTransition-enter-from{
+  opacity: 0;
+}
+.modalTransition-enter-active{
+  transition: all 1s;
+}
+.modalTransition-enter-to{
+  opacity: 1;
+}
+.modalTransition-leave-from{
+  opacity: 1;
+}
+.modalTransition-leave-active{
+  transition: all 1s;
+}
+.modalTransition-leave-to{
+  opacity: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -71,31 +155,8 @@ body {
   font-weight: bold;
   cursor: pointer;
 }
-.room-img {
-  width: 100%;
-  margin-top: 40px;
-}
-.black-bg {
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  position: fixed;
-  padding: 20px;
-}
-.white-bg {
-  width: 100%;
-  background: whitesmoke;
-  border-radius: 8px;
-  padding: 20px;
-  
-}
-.modal-close-btn {
-  width: 50px;
-  margin: 20px;
-  padding: 5px 10px;
-  cursor: pointer;
-  display: block;
-}
+
+
 
 
 </style>
