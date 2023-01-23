b0a2df41e92cf69bb5164de51e3cd9b8ad6ae1c8
jamie7dev <jamie7dev@gmail.com>
2023-01-23 17:52:00 +0900

    feature: seperate component 'Card.vue'

---
+++ src/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <img :src="room.image">
+    <h4 @click="$emit('openModal')">{{ room.title }}</h4>
+    <p>{{room.price}}원</p>
+  </div>
+  
+  
+ 
+</template>
+
+<script>
+export default {
+  //eslint-disable-next-line
+  name : 'Card',
+  props : {
+    room: Object,
+  }
+}
+</script>
+
+<style>
+
+</style>
